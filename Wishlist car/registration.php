7d20a4e2965eb0acb595cc5b9c5deddb61c5798a
ndkhair24-mu <22070920@vnu.edu.vn>
2025-11-06 16:59:16 +0700

    Update registration.php

--- Wishlist car/registration.php
+++ Wishlist car/registration.php
@@ -7,8 +7,7 @@
         
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
-        <link rel="stylesheet" href="../assets/font/fontawesome-free-7.0.0-web/fontawesome-free-7.0.0-web/css/all.min.css">
-        
+
         <link rel="stylesheet" href="registration.css"> 
     </head>
 
