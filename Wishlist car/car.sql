a5f0595a0d0aca8de457ec20c30055007ba110e5
Nguyen Tuan Thang <22070969@vnu.edu.vn>
2025-11-06 14:25:15 +0700

    Update car.sql

--- Wishlist car/car.sql
+++ Wishlist car/car.sql
@@ -37,16 +37,23 @@ VALUES
 ('minh', '2000-03-25', 'minhpass', 'minh@gmail.com', 'Vietnam', '0905123123'),
 ('alex', '1992-11-30', 'alex321', 'alex@hotmail.com', 'UK', '0845123456'),
 ('sakura', '1999-09-09', 'sakura99', 'sakura@jp.com', 'Japan', '0812345678'),
-('maria', '1997-06-18', 'maria18', 'maria@es.com', 'Spain', '0823456789');
+('maria', '1997-06-18', 'maria18', 'maria@es.com', 'Spain', '0823456789'),
+('khaibeo', '1994-08-22', '890pass', 'khaibeo@us.com', 'Cambodia', '0978123456'),
+('namdo', '2001-02-14', 'namdo2025', 'namdo@vn.com', 'Vietnam', '0918123123'),
+('enzo', '1988-12-05', 'enzoF1', 'enzo@it.com', 'Italy', '0888999777');
 
 INSERT INTO cars (name, color, brand, price, year, user_id)
 VALUES
-('Toyota Camry', 'White', 'Toyota', 750000.00, 2020, 1),
-('Honda Civic', 'Black', 'Honda', 680000.00, 2019, 2),
-('Mazda CX5', 'Red', 'Mazda', 820000.00, 2021, 3),
-('Hyundai Tucson', 'Blue', 'Hyundai', 790000.00, 2022, 4),
-('Ford Ranger', 'Silver', 'Ford', 910000.00, 2018, 5),
-('Nissan Altima', 'Gray', 'Nissan', 850000.00, 2020, 6),
-('Kia Seltos', 'Orange', 'Kia', 730000.00, 2023, 7);
+('Toyota Camry', 'White', 'Toyota', 750000000, 2020, 1),
+('Honda Civic', 'Black', 'Honda', 680000000, 2019, 2),
+('Mazda CX5', 'Red', 'Mazda', 820000000, 2021, 3),
+('Hyundai Tucson', 'Blue', 'Hyundai', 790000000, 2022, 4),
+('Ford Ranger', 'Silver', 'Ford', 910000000, 2018, 5),
+('Nissan Altima', 'Gray', 'Nissan', 850000000, 2020, 6),
+('Kia Seltos', 'Orange', 'Kia', 730000000, 2023, 7),
+('Ferrari 812 ', 'Red', 'Ferrari', 12500000000, 2024, 8),
+('Lamborghini Aventador', 'Yellow', 'Lamborghini', 15000000000, 2023, 9),
+('Bugatti Chiron', 'Blue', 'Bugatti', 67000000000, 2022, 10);
+
 
 
