e09c1805a6a02e4f33212b9401f3d8c93211f746
22070955-alt <22070955@vnu.edu.vn>
2025-11-06 14:01:42 +0700

    n

--- Wishlist car/login/login.php
+++ Wishlist car/login/login.php
@@ -1,6 +1,6 @@
 <html lang="vi" xmlns="">
     <head>
-        <title>LOGIN</title>
+        <title>Login</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
