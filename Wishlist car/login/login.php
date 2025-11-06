2282ce56c5a57779edf79827ac4cf54eba307d5e
22070955-alt <22070955@vnu.edu.vn>
2025-11-06 12:01:25 +0700

    n

--- Wishlist car/login/login.php
+++ Wishlist car/login/login.php
@@ -5,8 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
-        <link rel="stylesheet" href="/Wishlist car/assets/font/fontawesome-free-7.0.0-web/fontawesome-free-7.0.0-web/css/all.min.css">
-        <link rel="stylesheet" href="/Wishlist car/login/login.css">
+        <link rel="stylesheet" href="../assets/font/fontawesome-free-7.0.0-web/fontawesome-free-7.0.0-web/css/all.min.css">
+        <link rel="stylesheet" href="login.css">
     </head>
 
     <body>
