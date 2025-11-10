2dd6457b3a58e5fc8f629a303858e0cdfcfab8a5
NguyenThanhLan1204 <22071014@vnu.edu.vn>
2025-11-10 22:13:32 +0700

    Update delete.php

--- Wishlist Cars/delete.php
+++ Wishlist Cars/delete.php
@@ -1,30 +1,27 @@
 <?php
-//creating a database connection - $link is a variable use for just connection class
 $link = mysqli_connect("localhost", "root", "") or die(mysqli_error($link));
 mysqli_select_db($link, "user_car_system") or die(mysqli_error($link));
 
-$id = $_GET["id"]; // ID of the car to delete
+$id = $_GET["id"]; 
 
-// Get car information (to display confirmation)
 $res = mysqli_query($link, "SELECT * FROM cars WHERE id=$id");
 $car = mysqli_fetch_array($res);
 
 
-// When the user confirms deletion
 if (isset($_GET["confirm"]) && $_GET["confirm"] == "yes") {
     $delete_query = "DELETE FROM cars WHERE id=$id";
     mysqli_query($link, $delete_query) or die(mysqli_error($link));
-    header("Location: home.php"); // Redirect back to the car list
+    echo "<script>
+            alert('Car removed successfully!');
+            window.location.href = 'home.php';
+          </script>";
     exit;
 }
-?>
 
-<!-- Confirmation page -->
-<h2>Are you sure you want to remove this car from your wishlist?</h2>
-
-<button>
-  <a href="delete.php?id=<?php echo $id ?>&confirm=yes">Yes</a>
-</button>
-<button>
-  <a href="home.php">No</a>
-</button>
+echo "<script>
+        if (confirm('Are you sure you want to remove this car from your wishlist?')) {
+            window.location.href = 'delete.php?id=$id&confirm=yes';
+        } else {
+            window.location.href = 'home.php';
+        }
+      </script>";
