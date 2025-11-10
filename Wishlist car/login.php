bdb451ccbffeecfd2370990a4a4c476e91f80c29
NguyenAnhMy242712 <nguyenanhmy242712@gmail.com>
2025-11-10 19:34:46 +0700

    Update login.php

--- Wishlist car/login.php
+++ Wishlist car/login.php
@@ -12,7 +12,7 @@ if (isset($_SESSION['username'])) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
-        <link rel="stylesheet" href="./css/login.css">
+        <link rel="stylesheet" href="./css/login.css"> 
     </head>
 
     <body>
